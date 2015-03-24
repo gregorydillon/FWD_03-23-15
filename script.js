132f679d642a27cdce0f344e37b794f800c34b38
Gregory Dillon <gregorydillon@gmail.com>
2015-03-23 21:01:37 -0700

    did a lot of typing, but its not yet working

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-$('textarea').on('keyup', function () {
-  var remaining = 140 - $(this).val().length;
+var textarea = $('textarea');
+var tweetCounter = $('.tweet-counter');
+var submitButton = $('input');
+var tweetList = $('.tweets');
+var firstTweet = $('.tweet').first;
 
-  $('.tweet-counter').text(remaining);
-
+var calculateRemaining = function(){
+  var remaining = 140 - $textarea.val().length;
+  // var remaining = 140 - $(this).val().length;
+  tweetCounter.text(remaining);
   $('input').prop('disabled', remaining < 0);
+};
+
+$('textarea').on('keyup', calculateRemaining);
+
+
+
+$('form').on('submit', function (event){
+  event.preventDefault();
+  
+  var newTweet = $('.tweet').first().clone();
+  
+  $newTweet.find('tweet-content').text(textarea.val());
+  
+  $('tweets').prepend(newTweet);
+  
+  
+  textarea.val('');
+  calculateRemaining();
 });
