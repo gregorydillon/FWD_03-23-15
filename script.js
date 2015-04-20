c00a74ab0e3dc3968f131da9603ec4e4f7521c24
Gregory Dillon <gregorydillon@gmail.com>
2015-04-19 22:43:12 -0700

    3 files modified

--- script.js
+++ script.js
@@ -2,15 +2,23 @@ var textarea = $('textarea');
 var tweetCounter = $('.tweet-counter');
 var submitButton = $('input');
 var tweetList = $('.tweets');
-var firstTweet = $('.tweet').first;
+var firstTweet = $('.tweet').first();
 
 var calculateRemaining = function(){
-  var remaining = 140 - $textarea.val().length;
+  var remaining = 140 - textarea.val().length;
   // var remaining = 140 - $(this).val().length;
   tweetCounter.text(remaining);
-  $('input').prop('disabled', remaining < 0);
+  if (remaining < 15){
+    tweeetCounter.addClass('warning');
+    }else{
+      tweetCounter.removeClass('warning');
+    }
+    
+    
+  
+  $('input').prop('disabled', remaining < 0 || remaining === 140);
 };
-
+calculateRemaining();
 $('textarea').on('keyup', calculateRemaining);
 
 
@@ -20,7 +28,7 @@ $('form').on('submit', function (event){
   
   var newTweet = $('.tweet').first().clone();
   
-  $newTweet.find('tweet-content').text(textarea.val());
+  newTweet.find('tweet-content').text(textarea.val());
   
   $('tweets').prepend(newTweet);
   
@@ -28,3 +36,14 @@ $('form').on('submit', function (event){
   textarea.val('');
   calculateRemaining();
 });
+
+$('button').on('click',function(){
+  var $this = $(this);
+  if ($this.hasClass('favorite')){
+    $(this).closest('.tweet').addClass('favorited');
+  }
+});
+
+if($(this).hasClass('retweet')){
+  $this.closest('tweet').addClass('retweeted');
+}
